23d3d2f8ad50969ce96b151d62272488024e01ce
EbanNox <108201516+EbanNox@users.noreply.github.com>
2024-03-05 15:08:56 +0100

    Uppdated plugin formatting for current support

--- mov_cli_sflix/__init__.py
+++ mov_cli_sflix/__init__.py
@@ -1,9 +1,16 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from mov_cli.plugins import PluginHookData
+
 from .scraper import *
 
-plugin = {
+plugin: PluginHookData = {
+    "version": 1,
     "scrapers": {
-        "sflix": sflixProvider
+        "sflix": SflixProvider
     }
 }
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
